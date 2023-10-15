c8545914131935d7b87c00c514e89e8014f71295
RambunctiousCookie <jason.tay.nw@gmail.com>
2023-10-15 19:28:25 +0800

    Change code to allow for modularity in reference table
    - Allow user to set their own custom reference table
    - Remove static methods to compensate

--- src/main/java/org/example/OffsetCipher.java
+++ src/main/java/org/example/OffsetCipher.java
@@ -3,18 +3,27 @@ package org.example;
 import java.util.*;
 
 public class OffsetCipher implements Cipher {
-    private static final Character[] referenceTable = new Character[] {
-            //inline char array for faster instantiation than list, plus it is a size-immutable reference table
-            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-            'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
-            '(', ')', '*', '+', ',', '-', '.', '/'
-    };
-    private static final Set<Character> legalCharactersSet = new HashSet<>(Arrays.asList(referenceTable));
-    public OffsetCipher() {/*empty constructor*/}
+    private Character[] referenceTable;
+    private Set<Character> legalCharactersSet;
+    public OffsetCipher() {
+        referenceTable = new Character[] {
+                //inline char array for faster instantiation than list, plus it is a size-immutable reference table
+                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+                '(', ')', '*', '+', ',', '-', '.', '/'
+        };
+        legalCharactersSet = new HashSet<>(Arrays.asList(referenceTable));
+
+    }
     public Character[] getReferenceTable() {
         return referenceTable;
     }
+    public void setReferenceTable(Character[] referenceTable) {
+        this.referenceTable = referenceTable;
+        this.legalCharactersSet = new HashSet<>(Arrays.asList(referenceTable));
+    }
+
     public char getOffset(String target) {
         char offset = target.toCharArray()[0];
         if (!isCharValid(offset))
@@ -57,7 +66,7 @@ public class OffsetCipher implements Cipher {
         return mapper;
     }
 
-    private static String constructProcessedString(char[] unprocessedCharArray, Map<Character,Character> mapper){
+    private String constructProcessedString(char[] unprocessedCharArray, Map<Character,Character> mapper){
         StringBuilder decodedString = new StringBuilder();
         for (int i=0;i<unprocessedCharArray.length;i++){
             if(isCharValid(unprocessedCharArray[i]))
@@ -68,7 +77,7 @@ public class OffsetCipher implements Cipher {
         return decodedString.toString();
     }
 
-    private static int findIdxInReferenceTable(Character character){
+    private int findIdxInReferenceTable(Character character){
         for (int i=0;i<referenceTable.length;i++){
             if (referenceTable[i] == character)
                 return i;
@@ -76,11 +85,12 @@ public class OffsetCipher implements Cipher {
         throw new IllegalArgumentException("Input character array is not valid given the reference array.");
     }
 
-    private static boolean isCharValid(Character target){
+    private boolean isCharValid(Character target){
         return legalCharactersSet.contains(target);
+        //return Arrays.stream(referenceTable).anyMatch(element -> element == target);
     }
 
-    private static void displayMapper(Map<Character,Character> mapper){
+    private void displayMapper(Map<Character,Character> mapper){
         mapper.entrySet().stream().sorted(Comparator.comparingInt(entry -> findIdxInReferenceTable(entry.getKey()))).forEach(System.out::println);
     }
 }
--- src/test/java/org/example/OffsetCipherTest.java
+++ src/test/java/org/example/OffsetCipherTest.java
@@ -4,6 +4,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -25,10 +28,19 @@ public class OffsetCipherTest {
         testRounds = 99;   //TODO: You can set the number of rounds/loops for the robustness test here.
     }
 
-//    @BeforeEach
-//    public void setupString() {
-//        testString = generateRandomString(15,65);
-//    }
+    @BeforeEach
+    public void setupString() {
+        offsetCipher = new OffsetCipher();
+        //Reset the reference table every time a test is run
+
+        ////Could be cheaper to just reset the table but more dangerous if there are more tests/variables are added down the road
+//        offsetCipher.setReferenceTable(new Character[] {
+//                'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+//                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+//                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+//                '(', ')', '*', '+', ',', '-', '.', '/'
+//        });
+    }
 
     @Test
     public void testEncodeDecodeWithGivenLetterB() {
@@ -50,7 +62,6 @@ public class OffsetCipherTest {
 
     @Test
     public void testEncodeDecodeRobustness() {
-
         for (int i = 0; i < testRounds; i++) {
             testString = generateRandomString(15, 65);
             String encodedString = offsetCipher.encode(testString);
@@ -58,6 +69,19 @@ public class OffsetCipherTest {
             assertEquals(testString, decodedString);
         }
     }
+
+    @Test
+    public void testEncodeDecodeModularity() {
+        for (int i = 0; i < testRounds; i++) {
+            offsetCipher.setReferenceTable(generateRandomCharArray(validKeyboardChars));
+
+            testString = generateRandomString(15, 65);
+            String encodedString = offsetCipher.encode(testString);
+            String decodedString = offsetCipher.decode(encodedString);
+            assertEquals(testString, decodedString);
+        }
+    }
+
 
     private String generateRandomString(int minLength, int maxLength) {
         if (minLength > maxLength || minLength < 0) {
@@ -77,5 +101,23 @@ public class OffsetCipherTest {
         }
 
         return randomString.toString();
+    }
+
+    private Character[] generateRandomCharArray(String validKeyboardChars) {
+        Random random = new Random();
+        int length = random.nextInt(44) + 1;    //random number between 0 (inclusive) and 44 (exclusive)
+        List<Character> charList = new ArrayList<>();
+
+        for (int i = 0; i < length; i++) {
+            char randomChar;
+            do {
+                randomChar = validKeyboardChars.charAt(random.nextInt(validKeyboardChars.length()));
+            } while (charList.contains(randomChar));
+
+            charList.add(randomChar);
+        }
+
+        Collections.shuffle(charList);
+        return charList.toArray(new Character[0]);
     }
 }
